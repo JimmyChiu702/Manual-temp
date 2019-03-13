9f27ad09510c0a645c61cdd4ad4ed9705617f186
104062334 <jimmychiu702@gmail.com>
2019-03-13 10:38:31 +0800

    2019-03-13 workshop ui finished

--- client/workshop/src/components/Main.jsx
+++ client/workshop/src/components/Main.jsx
@@ -23,6 +23,49 @@ export default class Main extends React.Component {
             }
         }
 
+        this.content = [
+            {
+                videoUrl: "https://www.youtube.com/watch?v=qy5ldGXjHOg",
+                questions: [
+                    {
+                        description: "書面審查的目的，是為了比較和排序考生。",
+                        options: [
+                            "正確",
+                            "不正確"
+                        ],
+                        answer: {
+                            option: 'B',
+                            description: "答案(B)，書面審查的目的，不是為了比較和排序考生，而是以學系（班）的審查共識，鑑別出最適合學系（班）招生特色與需求的考生。"
+                        }
+                    },{
+                        description: "以下對「書面審查評量尺規」的說明，何者有誤？",
+                        options: [
+                            "是審查委員依據學系（班）的招生需求而共同制定。",
+                            "是學系（班）的審查評分共識。",
+                            "僅作為學系（班）審查使用，無須對外公告。",
+                            "藉由審查評量尺規，審查委員能準確且公平地鑑別考生。"
+                        ],
+                        answer: {
+                            option: 'C',
+                            description: "答案(C)，審查評量尺規的部分內容，將於109學年度對外公告，以協助考生瞭解學系（班）的審查標準和準備方向。"
+                        }        
+                    },{
+                        description: "以下針對書面審查流程SOP中之目的與重要執行事項的說明，何者正確？",
+                        options: [
+                            "審查前，最重要的是建立分組共識與評分共識。",
+                            "審查前會議中，學系（班）確認每一位審查委員都已瞭解尺規內涵的過程，稱為「分組共識」。",
+                            "審查期間，審查委員應獨立審查，避免有任何的意見交流，以免影響評分結果。",
+                            "審查後，若考生的成績有差異，學系（班）應尊重個別審查委員的評分，無須討論或調整分數。"
+                        ],
+                        answer: {
+                            option: 'C',
+                            description: "答案(C)，審查前會議中，學系（班）確認每一位審查委員都已瞭解尺規內涵的過程，稱為「尺規校準」，是指審查委員以尺規試評、並確認尺規的評分面向與尺度。"
+                        }
+                    }
+                ]
+            }
+        ];
+
         this.handleTopicChange = this.handleTopicChange.bind(this);
     }
 
@@ -52,15 +95,12 @@ export default class Main extends React.Component {
                         <Tab label='主題四' />
                     </Tabs>
                 </Paper>
-                {this.state.topicNum==0 && <Topic topicNum={0} />}
-                {this.state.topicNum==1 && <Topic topicNum={1} />}
-                {this.state.topicNum==2 && <Topic topicNum={2} />}
-                {this.state.topicNum==3 && <Topic topicNum={3} />}
+                {this.state.topicNum==0 && <Topic content={this.content[0]} />}
             </div>
         );
     }
 
     handleTopicChange(event, num) {
-        this.setState({topicNum: num}, ()=>{console.log(num)})
+        this.setState({topicNum: num});
     }
 }
--- client/workshop/src/components/Topic.jsx
+++ client/workshop/src/components/Topic.jsx
@@ -1,17 +1,121 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import TextField  from '@material-ui/core/TextField';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
+import Button from '@material-ui/core/Button';
+import Collapse from '@material-ui/core/Collapse';
+
+import { Player } from 'video-react';
+
+import "../../node_modules/video-react/dist/video-react.css";
 import './Topic.css';
 
 export default class Topic extends React.Component {
+    static propTypes = {
+        content: PropTypes.object
+    };
+
     constructor(props) {
         super(props);
+        
+        this.state = {
+            userInfo: null,
+            userAns: null,
+            userOpinion: '',
+            isAnsDisplayed: false,
+            isAnsSubmitted: false
+        }
+
+        this.queNum = ['一', '二', '三', '四', '五', '六', '七', '八', '九', '十'];
+        this.optNum = ['A', 'B', 'C', 'D', 'E'];
+
+        this.handleAnsChange = this.handleAnsChange.bind(this);
+        this.handleOpinionInputChange = this.handleOpinionInputChange.bind(this);
+        this.handleOpinionSubmit = this.handleOpinionSubmit.bind(this);
+        this.handleUserAnsSubmit = this.handleUserAnsSubmit.bind(this);
+        this.handleAnsOpen = this.handleAnsOpen.bind(this);
+    }
+
+    componentDidMount() {
+        let ans = [];
+        for (let i=0; i<this.props.content.questions.length; i++) {
+            ans.push('A');
+        }
+        this.setState({userAns: ans});
+        this.getUserInfo();
     }
 
     render() {
+        const content = this.props.content;
         return (
-            <div>
-                {this.props.topicNum}
+            <div className='topic-container'>
+                <div className='player-container'>
+                    <Player src={content.videoUrl} />
+                </div>
+                <div className='question-container'>
+                    <Typography className='secondary-title' variant='display1' color='textPrimary' gutterBottom>牛刀小試</Typography>
+                    {!!content.questions && !!this.state.userAns && content.questions.map((question, i) => (
+                        <div key={i} className='question'>
+                            <Typography className='question-dicription' variant='body1'>{`${this.queNum[i]}、${question.description}`}</Typography>
+                            <FormControl className='options-container'>
+                                <RadioGroup value={this.state.userAns[i]} onChange={(event) => {this.handleAnsChange(event.target.value, i)}}>
+                                    {question.options.map((opt, j) => (
+                                        <FormControlLabel value={this.optNum[j]} control={<Radio color="primary" />} label={`(${this.optNum[j]}) ${opt}`} key={j} />
+                                    ))}
+                                </RadioGroup>
+                            </FormControl>
+                            <Collapse in={this.state.isAnsDisplayed} className='answer-container'>
+                                <Typography color='inherit' style={{color: this.state.userAns[i]==question.answer.option?'limeGreen':'red'}}>{question.answer.description}</Typography>
+                            </Collapse>
+                        </div>
+                    ))}
+                    <Button variant="outlined" color="primary" className='confirm-btn' size='large' onClick={this.handleUserAnsSubmit} disabled={this.state.isAnsSubmitted}>送出</Button>
+                </div>
+                <form className='opinion-box-container'>
+                    <Typography className='secondary-title' variant='display1' color='textPrimary' gutterBottom>意見欄</Typography>
+                    <TextField value={this.state.userOpinion} 
+                            onChange={this.handleOpinionInputChange}
+                            label='寫下您寶貴的意見'
+                            margin='normal'
+                            variant='outlined'
+                            fullWidth />
+                    <Button variant="outlined" color="primary" className='confirm-btn' size='large' onClick={this.handleOpinionSubmit}>送出</Button> 
+                </form>
             </div>
         );
+    }
+
+    getUserInfo() {
+
+    }
+
+    handleAnsChange(ans, qNum) {
+        if (!this.state.isAnsSubmitted) {
+            let newAns = this.state.userAns.slice();
+            newAns[qNum] = ans;
+            this.setState({userAns: newAns});
+        }
+    }
+
+    handleOpinionInputChange(event) {
+        this.setState({userOpinion: event.target.value})
+    }
+
+    handleOpinionSubmit() {
+        this.setState({userOpinion: ''});
+    }
+
+    handleUserAnsSubmit() {
+        this.setState({isAnsSubmitted: true})
+        this.handleAnsOpen();
+    }
+
+    handleAnsOpen() {
+        this.setState({isAnsDisplayed: true});
     }
 }
